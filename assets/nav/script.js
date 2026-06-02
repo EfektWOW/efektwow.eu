88f5e3e15de507960998ac6586c5b12d64602fe0
Sacky <89039310+sackuspl@users.noreply.github.com>
2026-06-02 14:08:32 +0200

    dddssa

--- assets/nav/script.js
+++ assets/nav/script.js
@@ -2,3 +2,10 @@ window.addEventListener('scroll', function() {
     document.querySelector('.wow-header')
         .classList.toggle('wow-scrolled', window.scrollY > 30);
 });
+
+const navbar = document.querySelector('.wow-navbar');
+const toggle = document.querySelector('.wow-mobile-toggle');
+
+toggle.addEventListener('click', () => {
+    navbar.classList.toggle('wow-open');
+});
